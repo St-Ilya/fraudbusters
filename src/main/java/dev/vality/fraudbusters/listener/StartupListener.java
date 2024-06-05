3424adf1f03aaf484e0395779dff0cecf5d5cc66
Ilya <79799415+St-Ilya@users.noreply.github.com>
2024-06-05 10:27:55 +0300

    Update StartupListener (add sql inj).java

--- src/main/java/dev/vality/fraudbusters/listener/StartupListener.java
+++ src/main/java/dev/vality/fraudbusters/listener/StartupListener.java
@@ -186,3 +186,25 @@ public class StartupListener implements ApplicationListener<ContextRefreshedEven
     }
 
 }
+
+class SqlInjTest {
+    public static List<Employee> func(String name) {
+    String sql = "select * from employee where name ='" + name + "'";
+    Statement dataSource;
+    try (Connection c = dataSource.getConnection();
+    ResultSet rs = c.createStatement().executeQuery(sql)) {
+        List<Employee> employees = new ArrayList<>();
+        while (rs.next()) {
+            employees.add(new Employee(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getString("password"),
+            Role.valueOf(rs.getString("role"))
+            ));
+        }
+        return employees;
+    } catch (SQLException ex) {
+        throw new RuntimeException(ex);
+    }
+    }   
+}
